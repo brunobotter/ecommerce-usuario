18a463b604f4eaeff29d258f210f526c6ccacd36
brunobotter <62908563+brunobotter@users.noreply.github.com>
2024-06-26 19:37:22 -0300

    att database

--- config/MySql.go
+++ config/MySql.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/brunobotter/ecommerce-usuario/scheamas"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -8,7 +11,15 @@ import (
 
 func InitializeSql() (*gorm.DB, error) {
 	logger := GetLogger("mysql")
-	dsn := "admin:bruno171191@tcp(database-1.cfwgqgmc8vat.us-east-1.rds.amazonaws.com:3306)/produto?charset=utf8mb4&parseTime=true&loc=Local"
+	// Leia variáveis de ambiente
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	// Construir a string DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=true&loc=Local", dbUser, dbPassword, dbHost, dbName)
+
 	//create db and connection
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
